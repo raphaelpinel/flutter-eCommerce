053527985dc3e852b52d95373586f51c325376b9
Raphaël Pinel <raphaelpinel@gmail.com>
2019-10-14 10:04:15 +0300

    Create basic Register Page Layout

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -10,8 +10,23 @@ class RegisterPage extends StatefulWidget {
 class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('register'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Register',
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Register',
+              style: Theme.of(context).textTheme.headline,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
