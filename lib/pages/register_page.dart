7a86d2c7b437acdf7ebd8994e131b7f9552bbbf5
Raphaël Pinel <raphaelpinel@gmail.com>
2019-10-15 07:51:41 +0300

    add register page elements

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -16,15 +16,85 @@ class _RegisterPageState extends State<RegisterPage> {
           'Register',
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Register',
-              style: Theme.of(context).textTheme.headline,
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Register',
+                    style: Theme.of(context).textTheme.headline,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Username',
+                        hintText: 'Enter username, min length 6',
+                        icon: Icon(
+                          Icons.face,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                        hintText: 'Enter your email address',
+                        icon: Icon(
+                          Icons.face,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        hintText: 'Enter a password, min length 6 ',
+                        icon: Icon(
+                          Icons.face,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 20.0),
+                    child: Column(
+                      children: <Widget>[
+                        RaisedButton(
+                          child: Text('Submit',
+                              style: Theme.of(context).textTheme.body1.copyWith(
+                                    color: Colors.black,
+                                  )),
+                          onPressed: () {},
+                          color: Theme.of(context).primaryColor,
+                          elevation: 8.0,
+                        ),
+                        FlatButton(
+                          child: Text('Existing User? Login'),
+                          onPressed: () => print('login'),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ],
+          ),
         ),
       ),
     );
