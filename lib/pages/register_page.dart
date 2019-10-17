d14621a5a6f2acc4b5f7ea2592b9661480af2c28
Raphaël Pinel <raphaelpinel@gmail.com>
2019-10-17 12:04:08 +0300

    send register data to database

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({Key key}) : super(key: key);
@@ -106,8 +107,19 @@ class _RegisterPageState extends State<RegisterPage> {
     final form = _formKey.currentState;
     if (form.validate()) {
       form.save();
-      print('username: $_username, email: $_email, password: $_password');
+      _registerUser();
     }
+  }
+
+  void _registerUser() async {
+    http.Response response =
+        await http.post('http://localhost:1337/auth/local/register', body: {
+      "username": _username,
+      "email": _email,
+      "password": _password,
+    });
+    final responseData = json.decode(response.body);
+    print('responseData: $responseData');
   }
 
   @override
