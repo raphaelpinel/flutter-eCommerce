b6726c60ea626f1c0798220d4ab19905b85dda16
Raphaël Pinel <raphaelpinel@gmail.com>
2019-10-23 09:16:08 +0300

    style products page with appBar and gradient

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -1,8 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/models/app_state.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'dart:convert';
-import 'package:shared_preferences/shared_preferences.dart';
+
+final gradientBackground = BoxDecoration(
+    gradient: LinearGradient(
+        begin: Alignment.bottomLeft,
+        end: Alignment.topRight,
+        stops: [
+      0.1,
+      0.3,
+      0.5,
+      0.7,
+      0.9
+    ],
+        colors: [
+      Colors.deepOrange[300],
+      Colors.deepOrange[400],
+      Colors.deepOrange[500],
+      Colors.deepOrange[600],
+      Colors.deepOrange[700],
+    ]));
 
 class ProductsPage extends StatefulWidget {
   final void Function() onInit;
@@ -19,12 +36,53 @@ class _ProductsPageState extends State<ProductsPage> {
     widget.onInit();
   }
 
+  final _appBar = PreferredSize(
+    preferredSize: Size.fromHeight(60.0),
+    child: StoreConnector<AppState, AppState>(
+      converter: (store) => store.state,
+      builder: (context, state) {
+        return AppBar(
+            centerTitle: true,
+            title: SizedBox(
+              child: state.user != null ? Text(state.user.username) : Text(''),
+            ),
+            leading: Icon(Icons.store),
+            actions: [
+              Padding(
+                  padding: EdgeInsets.only(right: 12.0),
+                  child: state.user != null
+                      ? IconButton(
+                          icon: Icon(
+                            Icons.exit_to_app,
+                          ),
+                          onPressed: () => print('pressed'),
+                        )
+                      : Text(''))
+            ]);
+      },
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, AppState>(
       converter: (store) => store.state,
       builder: (context, state) {
-        return Text(json.encode(state.user));
+        return Scaffold(
+          appBar: _appBar,
+          body: Container(
+            decoration: gradientBackground,
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: [
+                    Text('Products Page'),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
       },
     );
   }
